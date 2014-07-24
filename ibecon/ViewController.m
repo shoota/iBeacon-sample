50c0af1fc9f05c25fed84daaa0a263aec71ccab3
shoota <shoota.dev@gmail.com>
2014-07-24 10:15:51 +0900

    iBeaconの動作確認

--- ibecon/ViewController.m
+++ ibecon/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) NSUUID *proximityUUID;
+@property (nonatomic) CLBeaconRegion *beaconRegion;
 
 @end
 
@@ -17,8 +22,90 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	if ([CLLocationManager isMonitoringAvailableForClass:[CLBeaconRegion class]]) {
+        
+        //locationManagerを生成してviewcontrollerにdelegate
+        self.locationManager = [CLLocationManager new];
+        self.locationManager.delegate = self;
+        
+        //UUID生成
+        self.proximityUUID = [[NSUUID alloc] initWithUUIDString:@"EE29A799-1E19-4315-993B-C30ACF58103F"];
+        
+        //Beacon領域生成
+        self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:self.proximityUUID
+                                                            identifier:@"org.anaguma.beacon"];
+        
+        // start
+        [self.locationManager startMonitoringForRegion:self.beaconRegion];
+        
+    }
 }
+
+// regionに入った時
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
+{
+    // ローカル通知
+    //[self sendLocalNotificationForMessage:@"Enter Region"];
+    
+    //region測定
+    if ([region isMemberOfClass:[CLBeaconRegion class]] && [CLLocationManager isRangingAvailable]) {
+        [self.locationManager startRangingBeaconsInRegion:(CLBeaconRegion *)region];
+    }
+}
+
+// regionから出たとき
+- (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
+{
+    // ローカル通知
+    //[self sendLocalNotificationForMessage:@"Exit Region"];
+    
+    // Beaconの距離測定を終了する
+    if ([region isMemberOfClass:[CLBeaconRegion class]] && [CLLocationManager isRangingAvailable]) {
+        [self.locationManager stopRangingBeaconsInRegion:(CLBeaconRegion *)region];
+    }
+}
+
+
+// locationManaerのイベントハンドリング
+- (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
+{
+    if (beacons.count > 0) {
+        CLBeacon *nearestBeacon = beacons.firstObject;
+        NSString *rangeMessage;
+        
+        // Beacon の距離でメッセージを変える
+        switch (nearestBeacon.proximity) {
+            case CLProximityImmediate:
+                rangeMessage = @"Range Immediate: ";
+                break;
+            case CLProximityNear:
+                rangeMessage = @"Range Near: ";
+                break;
+            case CLProximityFar:
+                rangeMessage = @"Range Far: ";
+                break;
+            default:
+                rangeMessage = @"Range Unknown: ";
+                break;
+        }
+        
+        // ローカル通知
+        NSString *message = [NSString stringWithFormat:@"major:%@, minor:%@, accuracy:%f, rssi:%ld",
+                             nearestBeacon.major, nearestBeacon.minor, nearestBeacon.accuracy, (long)nearestBeacon.rssi];
+        [self sendLocalNotificationForMessage:[rangeMessage stringByAppendingString:message]];
+    }
+}
+
+
+- (void)sendLocalNotificationForMessage:(NSString *)message
+{
+    UILocalNotification *localNotification = [UILocalNotification new];
+    localNotification.alertBody = message;
+    localNotification.fireDate = [NSDate date];
+    localNotification.soundName = UILocalNotificationDefaultSoundName;
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
